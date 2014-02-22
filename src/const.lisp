37941561f3f130b96f57a248b88d0ecd1d53ac42
Huang Ying <huang.ying.caritas@gmail.com>
2014-02-22 20:15:20 +0800

    Remove const.lisp
    const.lisp is not used now.

--- src/const.lisp
+++
@@ -1,6 +0,0 @@
-(in-package :gir)
-
-(defun get-const (info)
-  (let ((giarg-res (make-giarg)))
-    (constant-info-get-value info giarg-res)
-    (make-out (build-translator (constant-info-get-type info)) giarg-res)))
