21fd8eaad3d5465dac96936fc7ba249a781e37f3
Huang Ying <huang.ying.caritas@gmail.com>
2014-04-06 20:05:03 +0800

    function.lisp: Split converter from translator
    This is needed to implement array support.  Converter can be used to
    set/get/free array element, which is not giarg.

--- src/function.lisp
+++ src/function.lisp
@@ -19,86 +19,92 @@
       (:is-setter (return nil))
       (:wraps-vfunc (return nil)))))
 
+(defstruct
+    (converter
+      (:constructor make-converter (size set get free)))
+  size set get free)
+
 (defstruct 
     (translator 
       (:constructor make-translator (>giarg >value check description free)))
   >giarg >value check description free)
 
-(defun pointer->giarg (giarg value)
+(defun set-pointer (position value)
   (typecase value
-    (function (pointer->giarg giarg (funcall value :this)))
-    (t (setf 
-        (cffi:foreign-slot-value giarg '(:union argument) 'v-pointer) 
-        value))))
+    (function (set-pointer position (funcall value :this)))
+    (t (setf (cffi:mem-ref position :pointer) value))))
+
+(defun get-pointer (position)
+  (cffi:mem-ref position :pointer))
+
+(defun pointer->giarg (giarg value)
+  (set-pointer (cffi:foreign-slot-pointer giarg '(:union argument) 'v-pointer)
+	       value))
 
 (defun giarg->pointer (giarg)
-  (cffi:foreign-slot-value giarg '(:union argument) 'v-pointer))
+  (get-pointer (cffi:foreign-slot-pointer giarg '(:union argument) 'v-pointer)))
 
 (defun dont-free (giarg)
   (declare (ignore giarg)))
 
-(defun build-translator (type)
+(defun build-converter (type)
   (let* ((tag (type-info-get-tag type))
          (pointer? (type-info-is-pointer type))
-         (field (case tag
-                  (:boolean 'v-boolean)
-                  (:int8 'v-int8)
-                  (:uint8 'v-uint8)
-                  (:int16 'v-int16)
-                  (:uint16 'v-uint16)
-                  (:int32 'v-int32)
-                  (:uint32 'v-uint32)
-                  (:int64 'v-int64)
-                  (:uint64 'v-uint64)
-                  (:short 'v-short) 
-                  (:ushort 'v-ushort)
-                  (:int 'v-int)
-                  (:uint 'v-uint)
-                  (:long 'v-long)
-                  (:ulong 'v-ulong)
-                  (:ssize 'v-long)
-                  (:size 'v-ulong)
-                  (:float 'v-float)
-                  (:double 'v-double)
-                  (:time-t 'v-long)
-                  (:gtype 'v-ulong)
-                  (:utf8 'v-string)
-                  (:interface 'v-int)
-                  (:filename 'v-string)
-		  (:unichar 'v-uint32)
-                  (t 'v-pointer)))
-         (value->giarg
+	 (foreign-type (if pointer? :pointer
+			   (case tag
+			     (:void :pointer)
+			     (:boolean :boolean)
+			     (:int8 :int8)
+			     (:uint8 :uint8)
+			     (:int16 :int16)
+			     (:uint16 :uint16)
+			     (:int32 :int32)
+			     (:uint32 :uint32)
+			     (:int64 :int64)
+			     (:uint64 :uint64)
+			     (:short :short)
+			     (:ushort :ushort)
+			     (:int :int)
+			     (:uint :uint)
+			     (:long :long)
+			     (:ulong :ulong)
+			     (:sszie :long)
+			     (:szie :ulong)
+			     (:float :float)
+			     (:double :double)
+			     (:time-t :long)
+			     (:gtype :ulong)
+			     (:utf8 :pointer)
+			     (:interface :uint)
+			     (:filename :pointer)
+			     (:unichar :int32)
+			     (t :pointer))))
+	 (size (cffi:foreign-type-size foreign-type))
+         (set
+	  (if pointer?
+	      (case tag
+		((:utf8 :filename)
+		 (lambda (position value)
+		   (let ((pointer (if value (cffi:foreign-string-alloc value)
+				      (cffi:null-pointer))))
+		     (set-pointer position pointer))))
+		(:interface
+		 (lambda (position value)
+		   (set-pointer position (if value value (cffi:null-pointer)))))
+		(t #'set-pointer))
+	      (case tag
+		(:void (lambda (position value)
+			 (declare (ignore value))
+			 (set-pointer position (cffi:null-pointer))))
+		(t (lambda (position value)
+		     (setf (cffi:mem-ref position foreign-type) value))))))
+         (get
            (if pointer?
                (case tag
                  ((:utf8 :filename)
-                  (lambda (giarg value)
-		    (let ((pointer (if value (cffi:foreign-string-alloc value)
-				       (cffi:null-pointer))))
-		      (setf (cffi:foreign-slot-value
-			     giarg '(:union argument) 'v-string) pointer))))
-		 (:interface
-		  (lambda (giarg value)
-		    (pointer->giarg giarg
-				    (if value value (cffi:null-pointer)))))
-                 (t #'pointer->giarg))
-               (case tag
-                 (:void (lambda (giarg value)
-                          (declare (ignore value))
-                          (setf (cffi:foreign-slot-value 
-                                 giarg '(:union argument) 'v-pointer) 
-                                (cffi:null-pointer))))
-                 (t (lambda (giarg value)
-                      (setf (cffi:foreign-slot-value 
-                             giarg '(:union argument) field) 
-                            value))))))
-         (giarg->value
-           (if pointer?
-               (case tag
-                 ((:utf8 :filename)
-                   (lambda (giarg)
-                     (cffi:foreign-string-to-lisp 
-                      (cffi:foreign-slot-value 
-                       giarg '(:union argument) 'v-string))))
+                   (lambda (position)
+                     (cffi:foreign-string-to-lisp
+		      (get-pointer position))))
 		 (:interface
 		  (let* ((info (type-info-get-interface type))
 			 (info-type (info-get-type info)))
@@ -107,24 +113,72 @@
 		       (let ((struct-object (if (eq info-type :struct)
 						(build-struct info)
 						(build-object info))))
-			 (lambda (giarg)
-			   (let ((this (cffi:foreign-slot-value
-					giarg '(:union argument) 'v-pointer)))
+			 (lambda (position)
+			   (let ((this (get-pointer position)))
 			     (if (cffi:null-pointer-p this) nil
 				 (funcall struct-object this))))))
-		      (t #'giarg->pointer))))
-                 (t #'giarg->pointer))
+		      (t #'get-pointer))))
+                 (t #'get-pointer))
                (case tag
-                 (:void (lambda (giarg) (declare (ignore giarg)) nil))
-                 (t (lambda (giarg)
-                      (cffi:foreign-slot-value 
-                       giarg '(:union argument) field))))))
-         (giarg-free
+                 (:void (lambda (position) (declare (ignore position)) nil))
+                 (t (lambda (position)
+		      (cffi:mem-ref position foreign-type))))))
+         (free
           (if (and pointer? (member tag '(:utf8 :filename)))
-              (lambda (giarg) (cffi:foreign-free 
-                               (cffi:foreign-slot-value 
-                                giarg '(:union argument) 'v-string)))
-              #'dont-free))
+              (lambda (position)
+		(cffi:foreign-free (get-pointer position)))
+              #'dont-free)))
+    (make-converter size set get free)))
+(export 'build-converter)
+
+(defun build-translator (type)
+  (let* ((tag (type-info-get-tag type))
+         (pointer? (type-info-is-pointer type))
+         (field (if pointer? 'v-pointer
+		    (case tag
+		      (:void 'v-pointer)
+		      (:boolean 'v-boolean)
+		      (:int8 'v-int8)
+		      (:uint8 'v-uint8)
+		      (:int16 'v-int16)
+		      (:uint16 'v-uint16)
+		      (:int32 'v-int32)
+		      (:uint32 'v-uint32)
+		      (:int64 'v-int64)
+		      (:uint64 'v-uint64)
+		      (:short 'v-short)
+		      (:ushort 'v-ushort)
+		      (:int 'v-int)
+		      (:uint 'v-uint)
+		      (:long 'v-long)
+		      (:ulong 'v-ulong)
+		      (:ssize 'v-long)
+		      (:size 'v-ulong)
+		      (:float 'v-float)
+		      (:double 'v-double)
+		      (:time-t 'v-long)
+		      (:gtype 'v-ulong)
+		      (:utf8 'v-string)
+		      (:interface 'v-uint)
+		      (:filename 'v-string)
+		      (:unichar 'v-uint32)
+		      (t 'v-pointer))))
+	 (converter (build-converter type))
+         (value->giarg
+	  (lambda (giarg value)
+	    (funcall (converter-set converter)
+		     (cffi:foreign-slot-pointer giarg '(:union argument) field)
+		     value)))
+         (giarg->value
+	  (lambda (giarg)
+	    (funcall (converter-get converter)
+		     (cffi:foreign-slot-pointer giarg '(:union argument)
+						field))))
+         (giarg-free
+	  (lambda (giarg)
+	    (funcall (converter-free converter)
+		     (cffi:foreign-slot-pointer giarg '(:union argument)
+						field))))
          (check-value
           (lambda (value) (declare (ignore value)) t))
          (description (format nil "~a" tag)))
