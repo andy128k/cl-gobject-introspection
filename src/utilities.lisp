8046ef28ef88f1a0190bf5e85f913e5771129e22
Huang Ying <huang.ying.caritas@gmail.com>
2014-04-07 07:56:32 +0800

    Remove argv utilities functions
    Because we have array support now.

--- src/utilities.lisp
+++ src/utilities.lisp
@@ -1,20 +1,5 @@
 (in-package :gir)
 
-(defun argv-alloc (args)
-  (let* ((argc (length args))
-	 (argv (cffi:foreign-alloc :pointer :count argc)))
-    (loop
-       :for i :below argc
-       :for arg in args
-       :do (setf (cffi:mem-aref argv :pointer i)
-		 (cffi:foreign-string-alloc arg)))
-    argv))
-
-(defun argv-to-list (argc argv)
-  (loop
-     :for i :below argc
-     :collect (cffi:foreign-string-to-lisp (cffi:mem-aref argv :pointer i))))
-
 (defun zero-memory (position length)
   (loop
      :for i :below length
