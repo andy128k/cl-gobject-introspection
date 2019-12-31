ea2f8270dba8033148f092898312446b8852cb44
Andrey Kutejko <andy128k@gmail.com>
2019-12-31 17:06:56 +0100

    Fix array length detection in in-arg-clear

--- src/function.lisp
+++ src/function.lisp
@@ -850,7 +850,7 @@
 	       (if is-array-type
 		   (copy-find-set-c-array-type-length 
                       type
-                      (slot-value length-arg 'value))
+                      (when length-arg (slot-value length-arg 'value)))
 		   type)))
 	  (mem-free giarg real-type))))))
 
