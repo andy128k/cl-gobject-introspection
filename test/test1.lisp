3333cd2b1a9017f9d284ddb510dbfb8ee24eac0b
Andrey Kutejko <andy128k@gmail.com>
2020-12-29 23:00:16 +0100

    Fix test/test1.lisp

--- test/test1.lisp
+++ test/test1.lisp
@@ -2,37 +2,43 @@
 
 (in-package :gir-test)
 
+(defun load-typelib (file-name)
+  (iter (for dir-path in (gir:repository-get-search-path))
+	(let* ((file-path (format nil "~a/~a" dir-path file-name))
+	       (content (read-file file-path)))
+	  (when content
+	    (return content)))))
+
 #+nil
 (progn
-  (pprint (gir:repository-get-default))
-  (pprint (gir:repository-get-search-path))
-  
-  (let ((file (read-file "/usr/lib/girepository-1.0/Pango-1.0.typelib")))
-    (with-typelib
-	tlb file 
+  (format t "default: ~A~%" (gir:repository-get-default))
+  (format t "search paths: ~A~%" (gir:repository-get-search-path))
+
+  (let ((file (load-typelib "Pango-1.0.typelib")))
+    (with-typelib tlb file 
+      
+      (format t "namespace: ~A~%" (typelib-namespace tlb))
+      (format t "pango_version_string: ~A~%" (typelib-symbol tlb "pango_version_string"))
+      
+      (let ((repo (repository-new)))
+	(format t "typelib: ~A~%" (repository-load-typelib repo tlb :lazy))
 	
-	(pprint (typelib-namespace tlb))
-	(pprint (typelib-symbol tlb "pango_version_string"))
-	
-	(let ((repo (repository-new)))
-	  (pprint (repository-load-typelib repo tlb :lazy))
-	  
-	  (let ((i (repository-find-by-name repo "Pango" "Context")))
-	    (pprint (info-get-type i))
-	    (pprint (info-get-name i))
-	    (pprint (info-get-namespace i))
-	    (pprint (info-get-attributes i))
-	    (pprint (info-get-container i))
-	    (pprint (info-get-typelib i))
-	    )
+	(let ((i (repository-find-by-name repo "Pango" "Context")))
+	  (format t "Context's type: ~A~%" (info-get-type i))
+	  (format t "Context's name: ~A~%" (info-get-name i))
+	  (format t "Context's namespace: ~A~%" (info-get-namespace i))
+	  (format t "Context's attributes: ~A~%" (info-get-attributes i))
+	  (format t "Context's container: ~A~%" (info-get-container i))
+	  (format t "Context's typelib: ~A~%" (info-get-typelib i))
+	  )
 
-	  (let ((i (repository-find-by-name repo "Pango" "version_string")))
-	    (pprint (callable-info-get-args i))
-	    )
+	(let ((i (repository-find-by-name repo "Pango" "version_string")))
+	  (format t "version_string's args: ~A~%" (callable-info-get-args i))
+	  )
 
-	  (pprint (repository-get-dependencies repo "Pango"))
-	  (pprint (repository-get-loaded-namespaces repo))
-	  (pprint (repository-get-infos repo "Pango"))
+	(format t "dependencies: ~A~%" (repository-get-dependencies repo "Pango"))
+	(format t "namespaces: ~A~%" (repository-get-loaded-namespaces repo))
+	(format t "infos: ~A~%" (repository-get-infos repo "Pango"))
 
-	  ))))
+	))))
 
