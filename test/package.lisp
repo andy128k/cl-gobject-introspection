45d2cceceb44d55761e2221f30276e2eedc68d9c
Andrey Kutejko <andy128k@gmail.com>
2020-12-29 23:00:16 +0100

    Fix test/test1.lisp

--- test/package.lisp
+++ test/package.lisp
@@ -6,6 +6,8 @@
   (:use :common-lisp :iterate :gir :fiveam :cffi)
   (:export :main))
 
+(in-package :gir-test)
+
 (defun read-file (filename)
   (with-open-file (stream filename :element-type '(unsigned-byte 8))
     (let ((v (make-array (list (file-length stream)) :element-type '(unsigned-byte 8))))
